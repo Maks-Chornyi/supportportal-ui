697857f8ac5ad889b8388fdb489a8ce014726662
Maks-Chornyi <--replace-all>
2022-03-08 20:58:37 +0200

    SP-12: added logic for user service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,10 +3,9 @@ import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
 import { User } from "../model/user"
 import {Observable} from "rxjs";
 import { Injectable } from '@angular/core';
+import {CustomHttpResponse} from "../model/custom-http-response";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class UserService {
   private host: string = environment.apiUrl;
 
@@ -24,8 +23,8 @@ export class UserService {
     return this.http.post<User>(`${this.host}/user/update`, formData);
   }
 
-  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
-    return this.http.get<User>(`${this.host}/user/reset-password/${email}`);
+  public resetPassword(email: string): Observable<CustomHttpResponse | HttpErrorResponse> {
+    return this.http.get<CustomHttpResponse>(`${this.host}/user/reset-password/${email}`);
   }
 
   public updateProfileImage(formData: FormData): Observable<HttpEvent<any> | HttpErrorResponse> {
@@ -36,7 +35,34 @@ export class UserService {
       });
   }
 
-  public deleteUser(userId: number): Observable<any | HttpErrorResponse> {
-    return this.http.delete<User>(`${this.host}/user/delete/${userId}`);
+  public deleteUser(userId: number): Observable<CustomHttpResponse | HttpErrorResponse> {
+    return this.http.delete<CustomHttpResponse>(`${this.host}/user/delete/${userId}`);
   }
+
+  public addUserToLocalCache(users: User[]): void {
+      localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  public getUserFromLocalStorage(): User[] {
+    if (localStorage.getItem('users')) {
+      return JSON.parse(localStorage.getItem('users'));
+    }
+    return null;
+  }
+
+  public createUserFormData(loggedInUsername: string, user: User, profileImage: File): FormData {
+    const formData = new FormData();
+    formData.append('currentUsername', loggedInUsername);
+    formData.append('firstName', user.firstName);
+    formData.append('lastName', user.lastName);
+    formData.append('username', user.username);
+    formData.append('email', user.email);
+    formData.append('role', user.role);
+    formData.append('profileImage', profileImage);
+    formData.append('isActive', JSON.stringify(user.isActive));
+    formData.append('isNonLocked', JSON.stringify(user.isNotLocked));
+
+    return formData;
+  }
+
 }
