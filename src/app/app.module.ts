0bfda0cf5bc130daa97c2f38f6663ecd1dbfc52f
Maks Chornyi <maksym.chornyi@oa.edu.ua>
2022-03-14 13:08:13 +0200

    SP-13: provided guards (#2)
    Co-authored-by: Maks-Chornyi <--replace-all>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AuthenticationService} from "./service/authentication.service";
-import {UserService} from "./service/user.service";
-import {AuthInterceptor} from "./interceptor/auth.interceptor";
+import { AuthenticationService } from "./service/authentication.service";
+import { UserService } from "./service/user.service";
+import { AuthInterceptor } from "./interceptor/auth.interceptor";
+import { AuthenticationGuard } from "./guard/authentication.guard";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import {AuthInterceptor} from "./interceptor/auth.interceptor";
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [ AuthenticationService, UserService,
+  providers: [ AuthenticationGuard, AuthenticationService, UserService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
